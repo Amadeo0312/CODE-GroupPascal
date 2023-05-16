b69e4d1174bea5b4355d575f98f9822a2280a323
John Mark P. Senening <johnmark.senening@cit.edu>
2023-05-16 18:33:10 +0800

    Move file content related files to designated folder

--- CODE_Interpreter/Content/SyntaxErrorHandler.cs
+++ CODE_Interpreter/Content/SyntaxErrorHandler.cs
@@ -1,6 +1,11 @@
-﻿namespace CODE_Interpreter.Content;
+﻿using Antlr4.Runtime;
+namespace CODE_Interpreter.Content;
 
-public class SyntaxErrorHandler
+public class SyntaxErrorHandler : BaseErrorListener
 {
-    
+    public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+    {
+        Console.Error.WriteLine($" ERR! line {line}:{charPositionInLine} {msg}");
+        Environment.Exit(1);
+    }
 }
